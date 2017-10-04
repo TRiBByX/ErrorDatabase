4b53d140ba612ede58890ac8c3faba499e9d01c9
TRiBByX <christoffer.ssj@gmail.com>
2017-10-04 15:02:37 +0200

    Added a database

--- main.py
+++ main.py
@@ -1,2 +1,29 @@
+from handlers import databasehandler
+import os
+
+
 def main():
-    pass
+    database = os.path.join('model/', 'errordb.db')
+
+    sql_create_table = """ CREATE TABLE IF NOT EXISTS errors (
+                                id integer PRIMARY KEY,
+                                description text,
+                                comment text,
+                                discovery_date text,
+                                last_work_date text,
+                                fixed_date text NOT NULL,
+                                cause text NOT NULL,
+                                discovered_by text,
+                                fixed_by text
+                            );
+                       """
+    conn = databasehandler.create_connection(database)
+
+    if conn is not None:
+        databasehandler.create_table(conn, sql_create_table)
+    else:
+        print('An error occured with the creation of the error table')
+
+
+if __name__ == '__main__':
+    main()
